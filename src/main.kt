9e6323080793d877cc9ae443e99d37972dfecff3
saido-noor <saidoomarnoor@gmail.com>
2022-04-13 09:27:10 +0300

    Arrays

---
+++ src/main.kt
@@ -0,0 +1,53 @@
+fun main() {
+     var numbers = arrayOf(12,33,33,535,22,20,24,30,332,23)
+     var sortedNums = numbers.sortedArrayDescending()
+    println(sortedNums.contentToString())
+    var names = arrayOf("name", "sharu", "Koli", "Ali")
+    println(names.sortedArray().contentToString())
+//    println(numbers.count())
+//    println(numbers.sum())
+//    println(numbers.maxOrNull())
+//    numbers.forEach { num ->
+////        println(saido*saido)
+//        println(Math.pow(num.toDouble(),4.0))
+//
+//    }
+
+
+
+
+//    println(numbers[3])
+//    var friends = arrayOf("Yasmin", "Lucy", "Omar")
+//    var country = arrayOf("Kenya", 520000000, 65, "Nairobi", "Democracy", 0.95)
+//    println(country[4])
+
+//    var tableOne = arrayOf("Tessa", "Tut"," Angela","Maryann","Naima")
+//    var x = tableOne.get(6)
+//    println(tableOne[3])
+//    tableOne.set(3, "Brenda")
+//    println(tableOne[3])
+////    println(tableOne.contentToString())
+//     tableOne = tableOne.plus("Akuot")
+//    tableOne = tableOne.plus("Shamim")
+//    println(tableOne.contentToString())
+//    println(tableTwo.contentToString())
+//    var table2 = arrayOf("Akuot", "Shamim","Natete")
+//    var table3 = table2.plus(tableOne)
+//    println(table3.contentToString())
+//
+//    var index = tableOne.indexOf("Naima")
+//    println(index)
+
+//    var ang = tableOne[2]
+//    var mac = tableOne[4]
+//    tableOne.set(2,mac)
+//    tableOne.set(4,ang)
+//    println(tableOne.contentToString())
+}
+
+fun takesName(names:Array<String>){
+
+
+}
+//create a function takes in array of  name and print ou each of them in uppercase
+
